8d2d20fe5822f4b7fc6098bd6e14f5b539276153
balez4464 <balez4464@wrdsb.ca>
2020-09-15 14:05:39 +0000

    Created a program that tells user whic of his numbers are bigger

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *This program tells the user which of his numbers are bigger
+ * @author Zachary Balean
  */
 public class Main {
 
@@ -9,7 +10,24 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // Creates new scanner
+    Scanner input = new Scanner(System.in);
+
+    //Asks and allows user to input numberA
+    System.out.println("Please enter an integer:");
+    int numberA = input.nextInt();
+
+    //Asks and allows user to input numberB
+    System.out.println("Please enter another integer:");
+    int numberB = input.nextInt();
+
+    //Detects which number is biggest 
+    if(numberA > numberB){
+      System.out.println("The biggest number is " + numberA + ".");
+    } else if (numberA < numberB){
+      System.out.println("The biggest number is " + numberB + ".");
+    } else {
+      System.out.println("There is no biggest number.");
+    }
   }
 }
